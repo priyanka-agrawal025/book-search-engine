ab3201917748a0b78221216305a256c109441696
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-05-01 16:07:40 -0400

    completed back-end, still having issues with apollo server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./utils/auth')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +30,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
